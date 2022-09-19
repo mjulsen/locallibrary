b881eec4764dda1c57e134e88780e4614a0b5e38
mjulsen <46572954+mjulsen@users.noreply.github.com>
2022-09-19 18:02:00 -0500

    updated settings.py for deployment

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p($4t0a-zf-yykdn@83-!55r%%1iw%)l1g&9f*gfuyrxm%ajot'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
